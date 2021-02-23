57d3bbbe566f7f9d8dae356d1922ca0a69c4ddc1
bramov <alexey.abramov96@gmail.com>
2021-02-23 23:58:46 +0300

    refactor: change props to be destructuring

--- client/src/components/gameControllers/ActionButton.js
+++ client/src/components/gameControllers/ActionButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ActionButton = (props) => {
+const ActionButton = ({ start }) => {
   return (
     <>
-      <a onClick={props.start} className="btn-floating btn-large red pulse"><i className="material-icons">play_circle_outline</i></a>
+      <a onClick={start} className="btn-floating btn-large red pulse"><i className="material-icons">play_circle_outline</i></a>
       <h4>Начать новую игру</h4>
     </>
   )
