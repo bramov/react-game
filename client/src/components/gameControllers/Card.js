988515bfd0ff9996e1c3091a4af6732901b48538
bramov <alexey.abramov96@gmail.com>
2021-02-27 18:49:23 +0300

    feat: add basic game logic

--- client/src/components/gameControllers/Card.js
+++ client/src/components/gameControllers/Card.js
@@ -1,28 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const Card = (props) => {
-  const path = `/assets/images/classic/${props.type}.png`;
+const Card = ({ code, clickHandler, open }) => {
+  const path = `/assets/images/classic/${code}.png`;
   const pathBack = '/assets/images/purple_back.png';
-  const [isOpen, setOpen] = useState(false);
-  const classNaming = `card-front ${isOpen ? 'open' : ''}`;
-  const openCard = (e) => {
-    setOpen(true);
-    console.log(e.target.key);
-  }
-  return (
-    <div className="card-placer"
-         onClick={openCard}
-    >
-      <div className={classNaming}>
-        <div className="card-inner">
-          <img width="100%" src={pathBack} alt=""/>
 
-        </div>
-        <div className="card-outer">
-          <img width="100%" src={path} alt=""/>
+  return (
+      <div className="card-placer"
+           onClick={clickHandler}
+      >
+        <div className={`card-front ${open ? 'open' : ''}`}>
+          <div className="card-inner">
+            <img width="100%" src={pathBack} alt=""/>
+          </div>
+          <div className="card-outer">
+            <img width="100%" src={path} alt=""/>
+          </div>
         </div>
       </div>
-    </div>
   )
 };
 
--- client/src/components/gameControllers/GameScreen.js
+++ client/src/components/gameControllers/GameScreen.js
@@ -1,28 +1,165 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import Settings from "./Settings";
 import Card from "./Card";
 
 const GameScreen = () => {
-  const [state, setState] = useState({
-    open: [],
-    current: [],
-    score: 0,
-  })
-  const isCardSame = (el, prev) => el.type === prev.type;
-  const shuffleArray = (arr) => arr.sort(() => Math.round(Math.random() * 100) - 50);
-  const doubleAndShuffle = (arr) => shuffleArray([...arr, ...arr]);
-  const testArr = [{type: '10C'}, {type: '10H'}, {type: 'AS'}, {type: 'KC'}, {type: 'QH'}];
+  const [current, setCurrent] = useState(null);
+  const [previous, setPrevious] = useState(null);
+  const [cards, setCards] = useState([]);
+  const [active, setActive] = useState([]);
+
+  useEffect(() => {
+    checkIfCardsSame();
+
+  }, [current, previous]);
+
+  useEffect(() => {
+    const shouldBe = [
+      {
+        code: '10C',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: '10H',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'AS',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'KC',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'QH',
+        id: null,
+        open: false,
+        active: true
+      }
+    ]
+    let test = [...shouldBe, ...shouldBe];
+    const shuffleArray = (arr) => arr.sort(() => Math.round(Math.random() * 100) - 50);
+    test = test.map((el, i) => ({...el, id: i}))
+    const testArray = shuffleArray(test);
+    setCards(testArray);
+
+  }, []);
+
+  const playAgain = () => {
+    const shouldBe = [
+      {
+        code: '10C',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: '10H',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'AS',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'KC',
+        id: null,
+        open: false,
+        active: true
+      },
+      {
+        code: 'QH',
+        id: null,
+        open: false,
+        active: true
+      }
+    ]
+    let test = [...shouldBe, ...shouldBe];
+    const shuffleArray = (arr) => arr.sort(() => Math.round(Math.random() * 100) - 50);
+    test = test.map((el, i) => ({...el, id: i}))
+    const testArray = shuffleArray(test);
+    setCards(testArray);
+  }
+
+  const checkIfCardsSame = () => {
+    if (current && previous) {
+      if (current.code === previous.code) {
+        setCurrent(null);
+        setPrevious(null);
+        const updatedCards = cards
+          .map(card => card.id === previous.id ||
+            card.id === current.id ?
+            ({...card, active: false}) : card
+          )
+        setTimeout(() => {
+          setCards(updatedCards);
+        }, 500);
+      } else {
+        closeTwoCards();
+      }
+      console.log(cards);
+    }
+
+  }
+  const closeTwoCards = () => {
+    if (current && previous) {
+      const updatedCards = cards
+        .map(card =>
+          card.open === true && card.active === true ?
+          ({...card, open: false, active: true}) : card
+        );
+
+      setCurrent(null);
+      setPrevious(null);
+      setTimeout( () => {
+        setCards(updatedCards);
+      }, 500)
+
+    }
+  }
+  const openCard = (clickedCard) => {
+    if (!clickedCard.open) {
+      if (current) {
+        setPrevious(current);
+      }
+      setCurrent(clickedCard);
+
+      const updatedCards = cards
+        .map(card =>
+          card.id === clickedCard.id ?
+          ({...card, open: true, active: true}) : card
+        )
+      setCards(updatedCards);
+    }
+  }
 
   return (
     <>
       <div className="game-area container">
           <div className="row cards-wrapper">
-            {doubleAndShuffle(testArr).map(el => <Card type={el.type}/>)}
-          </div>
 
+            { cards.map(el => <Card
+                  {...el}
+                  clickHandler={() => openCard(el)}
+            />) }
+
+          </div>
       </div>
       <div className="settings-area">
-        <Settings/>
+        <Settings restartFunc={playAgain}/>
       </div>
     </>
   )
