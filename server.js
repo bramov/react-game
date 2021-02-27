1cfd8ad719569d3bd91eb75fd54a13eec40f7a08
bramov <alexey.abramov96@gmail.com>
2021-02-28 01:43:37 +0300

    feat: data loading from server

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
-
 const app = express();
 const path = __dirname + '/client/build/';
+
 app.use(express.static(path));
+const prepareDataSet = () => {
+  const codes = ['10C', '10H', 'AS', 'KC', 'QH'];
+  const res = codes.map(el => ({
+    code: el,
+    id: null,
+    open: false,
+    active: true
+  }))
+  return res;
+}
 
 app.get('/', async (req, res) => {
   try {
     res.sendFile(__dirname + path + 'index.html')
   } catch (e) {
-    res.status(500).json({ message: 'Похоже, что реакт не забилдился'})
+    res.status(500).json({ message: 'Похоже, билд реакта сломался'})
   }
 })
 app.get('/loadData', async (req, res) => {
   try {
-    const { data } = req.body;
-    console.log(req.body);
+    const data = prepareDataSet();
+    res.json(data)
   } catch(e) {
-    res.status(500).json({ message: 'Сервер не отработал запрос, возможно не переданы параметры?' })
+    res.status(500).json({ message: 'Ошибка сервера' })
   }
+
 })
-app.listen(process.env.PORT || 5000, () => console.log(`server started at ${process.env.PORT}`));
+app.listen(process.env.PORT || 5000, () => console.log(
+  `server started at ${process.env.PORT ? process.env.PORT : 5000}`));
