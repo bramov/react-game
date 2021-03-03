1fd70f71f30acd0e44a6087f9c44092eb87119d6
bramov <alexey.abramov96@gmail.com>
2021-03-03 05:37:50 +0300

    feat: add sounds at game

--- client/src/components/GameArea.js
+++ client/src/components/GameArea.js
@@ -1,36 +1,25 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import GameScreen from "./gameControllers/GameScreen";
 import ActionButton from "./gameControllers/ActionButton";
-import sound from "../audio/music.mp3";
-
+import useSound from "use-sound";
+import backgroundMusic from "../audio/music.mp3";
+import {useGlobalSettings} from "../utils/contextProvider";
 
 const GameArea = () => {
-  const [data, setData] = useState(null);
-  const [isActive, setActive] = useState(Boolean(localStorage.getItem('active')) || false);
-
-
-  useEffect(() => {
-    startMusic();
-  })
-  /* try to change later, not a 3rd party code */
-  const startMusic = () => {
-    const musicPlayer = document.getElementById('music');
-    musicPlayer.volume = 0;
-    musicPlayer.play();
-  }
+  const { musicValue } = useGlobalSettings();
+  const [isActive, setActive] = useState(false);
+  const [playBackgroundMusic] = useSound(backgroundMusic, {volume: musicValue / 100});
 
   const startGame = () => {
     setActive(true);
-    localStorage.setItem('active', 'true');
+    playBackgroundMusic();
   }
   return (
     <main>
       <div className="container game">
         { isActive ? <GameScreen/> : <ActionButton start={startGame}/>}
       </div>
-      <audio id="music" controls loop>
-        <source src={sound} type="audio/mpeg"/>
-      </audio>
+
     </main>
   )
 }
